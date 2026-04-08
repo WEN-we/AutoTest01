7f4de13213a7ad94bf77c7bd4b746324f94c2397
WEN-we <19892142453@139.com>
2026-04-08 14:49:58 +0800

    新增鸿蒙测试以完善全平台测试，项目结构初步成熟

---
+++ utils/harmony_driver.py
@@ -0,0 +1,26 @@
+import yaml
+import os
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+
+class HarmonyDriver:
+    @staticmethod
+    def get_driver():
+        config_path = os.path.join(os.path.dirname(__file__), "../config/harmony_config.yaml")
+        with open(config_path, "r", encoding="utf-8") as f:
+            cfg = yaml.safe_load(f)
+
+        options = UiAutomator2Options()
+        options.load_capabilities({
+            "platformName": "Harmony",
+            "deviceName": cfg["device_name"],
+            "udid": cfg["udid"],
+            "appPackage": cfg["app_package"],
+            "appActivity": cfg["app_activity"],
+            "noReset": True,
+            "automationName": "UiAutomator2"
+        })
+
+        driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+        driver.implicitly_wait(10)
+        return driver
